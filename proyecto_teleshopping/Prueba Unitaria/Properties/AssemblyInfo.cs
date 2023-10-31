fd02223c0a9a9ffe6b77f7eb5eb4e080dea3a0a3
michelleAspiazu26 <149460205+michelleAspiazu26@users.noreply.github.com>
2023-10-31 13:50:43 -0500

    prueba unitaria
    prueba unitaria

---
+++ proyecto_teleshopping/Prueba Unitaria/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prueba Unitaria")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prueba Unitaria")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1f93d50-8c19-45e9-9ecc-5db328db8f4e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
